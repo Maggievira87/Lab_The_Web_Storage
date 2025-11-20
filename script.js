491e309a1ff478ede02427a87684bbbfb0731681
Lorena <pontongeidy@gmail.com>
2025-11-20 11:09:48 -0500

    Cambios locales antes de cambiar de rama

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let currentPokemon = null;
+const pokeapi_url = "https://pokeapi.co/api/v2/pokemon/";
+const favorites_key = "pokeFavorites";
+const resultDiv = document.getElementById('result');
+const pokemonInput = document.getElementById('pokemonName');
+const favoritesDiv = document.getElementById('favoritos');
+const saveButton = document.getElementById('saveButton')
+
 // Buscar Pokémon usando la PokéAPI
 function searchPokemon(pokemonName) {
 
